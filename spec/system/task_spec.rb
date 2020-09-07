9d3414be8a2ad02eb32051b115cd0c77e68cbe1f
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-09-07 22:34:56 +0900

    [add]rspec #1

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   before do
     FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -27,6 +26,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
+         task = FactoryBot.create(:second_task)
+         visit task_path(task.id)
+         expect(page).to have_content 'Factoryで作ったデフォルトのコンテント２'
        end
      end
   end
