e177d3ffdcdc1768c318671cbbefeb09b53af06e
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-09-07 17:14:22 +0900

    [add]Rspec&system_spec #1

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :task do
-    
+    title { 'task_title' }
+    content { 'task_content' }
+  end
+  factory :second_task, class: Task do
+    title { 'Factoryで作ったデフォルトのタイトル２' }
+    content { 'Factoryで作ったデフォルトのコンテント２' }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,9 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.before(:each) do |example|
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: { args: ['no-sandbox'] } if example.metadata[:type] == :system
+  end
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
