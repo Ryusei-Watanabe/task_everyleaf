8dd12ee6725561f6efa9c8d36ff32e5fbbfa1ce4
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-09-17 21:31:37 +0900

    [add]label_sort #9

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,7 @@ class Task < ApplicationRecord
   enum priority: {High: 0, Medium: 1, Low: 2}
   scope :title_search, -> (title) { where('title LIKE(?)',"%#{title}%")}
   scope :state_search, -> (state) { where( state: "#{state}")}
+  scope :label_search, -> (label) { where( label_id: "#{label}")}
   scope :created_at_sort, -> {all.order( created_at: :desc)}
   scope :deadline_sort, -> {all.order( deadline: :desc)}
   scope :priority_sort, -> {all.order(priority: :asc)}
